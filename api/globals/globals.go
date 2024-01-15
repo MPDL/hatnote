994b5cac817827851f95495758b9375799846591
Felix Riehm <mail@felixriehm.de>
2024-01-15 14:14:41 +0100

    update frontend version

--- api/globals/globals.go
+++ api/globals/globals.go
@@ -6,4 +6,4 @@ const VERSION string = "20231210154026"
 // This has to be replaced manually by the developer. The prupose of this is to show a notification on the frontend
 // if there is a new frontend version. This might be helpful if the program runs on a long term installation or
 // if the user has to clear the cache to get the newest frontend version
-const EXPECTED_FRONTEND_VERSION int64 = 20231202174642
+const EXPECTED_FRONTEND_VERSION int64 = 20240115141327
